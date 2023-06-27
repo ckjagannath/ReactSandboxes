bfc08299bbbd26c4b6068be84d711fc00b600dda
Jagannath Chivukula <67112280+ckjagannath@users.noreply.github.com>
2023-06-27 10:59:16 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Hello World!</h1>
+    <ul>
+      <li>First</li>
+      <li>Second</li>
+      <li>Third</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
